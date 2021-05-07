58ed2d5cb0c8c9fef7b9740c7a51b06aa73d781f
OpenApps <32998735+openXapps@users.noreply.github.com>
2021-05-07 14:49:55 +0200

    Fixed indents and started with search option

--- src/components/Content/BookmarksComponent.js
+++ src/components/Content/BookmarksComponent.js
@@ -12,94 +12,94 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 import {
-    updateLastClicked,
-    getPopular,
-    getByCategory,
-    getFavourites,
+  updateLastClicked,
+  getPopular,
+  getByCategory,
+  getFavourites,
 } from '../../utilities/localstorage';
 import { useStyles } from './BookmarksStyles';
 
 const BookmarksComponent = ({ history }) => {
-    const theme = useTheme();
-    const breakpointSM = useMediaQuery(theme.breakpoints.down('sm'));
-    const classes = useStyles();
-    const { id } = useParams();
-    const [bookmarks, setBookmarks] = React.useState([]);
+  const theme = useTheme();
+  const breakpointSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const classes = useStyles();
+  const { id } = useParams();
+  const [bookmarks, setBookmarks] = React.useState([]);
 
-    React.useEffect(() => {
-        const route = history.location.pathname;
-        let data = {};
-        switch (route) {
-            case '/':
-                data = getPopular();
-                if (data.statusOK) setBookmarks(data);
-                break;
-            case '/favourites':
-                data = getFavourites();
-                if (data.statusOK) setBookmarks(data);
-                break;
-            default:
-                data = getByCategory(id);
-                if (data.statusOK) setBookmarks(data);
-                break;
-        }
-        // console.log('Bookmarks: route...', route);
-        // console.log('Bookmarks: id......', id);
-        // console.log('Bookmarks: data....', data);
+  React.useEffect(() => {
+    const route = history.location.pathname;
+    let data = {};
+    switch (route) {
+      case '/':
+        data = getPopular();
+        if (data.statusOK) setBookmarks(data);
+        break;
+      case '/favourites':
+        data = getFavourites();
+        if (data.statusOK) setBookmarks(data);
+        break;
+      default:
+        data = getByCategory(id);
+        if (data.statusOK) setBookmarks(data);
+        break;
+    }
+    // console.log('Bookmarks: route...', route);
+    // console.log('Bookmarks: id......', id);
+    // console.log('Bookmarks: data....', data);
 
-        // Clean-up function
-        return () => true;
-    }, [history.location.pathname, id]);
+    // Clean-up function
+    return () => true;
+  }, [history.location.pathname, id]);
 
-    const handleEdit = (e) => {
-        // console.log('Bookmarks: edit.siteId...', e.currentTarget.dataset.siteId);
-        const siteId = e.currentTarget.dataset.siteId;
-        history.push('/edit/' + siteId);
-    };
+  const handleEdit = (e) => {
+    // console.log('Bookmarks: edit.siteId...', e.currentTarget.dataset.siteId);
+    const siteId = e.currentTarget.dataset.siteId;
+    history.push('/edit/' + siteId);
+  };
 
-    const handleLastClicked = (e) => {
-        console.log(e.currentTarget.dataset.siteId);
-        updateLastClicked(e.currentTarget.dataset.siteId);
-    };
+  const handleLastClicked = (e) => {
+    console.log(e.currentTarget.dataset.siteId);
+    updateLastClicked(e.currentTarget.dataset.siteId);
+  };
 
-    return (
-        <Box width="100%" pl={{ sm: 1 }}>
-            <List disablePadding>
-                {bookmarks.statusOK ? (
-                    bookmarks.data.map((v, i) => {
-                        return (
-                            <div key={i}>
-                                <ListItem
-                                    disableGutters
-                                    button
-                                    component="a"
-                                    href={v.siteURL}
-                                    target="_blank"
-                                    rel="noopener"
-                                    data-site-id={v.siteId}
-                                    onClick={handleLastClicked}>
-                                    <ListItemText
-                                        className={classes.bookmarkText}
-                                        primary={v.siteName}
-                                        primaryTypographyProps={breakpointSM ? ({ variant: 'body1' }) : ({ variant: 'h5' })}
-                                        secondary={v.category ? v.category : null}
-                                    />
-                                    <ListItemSecondaryAction>
-                                        <IconButton
-                                            edge="end"
-                                            data-site-id={v.siteId}
-                                            onClick={handleEdit}
-                                        ><MoreVertIcon /></IconButton>
-                                    </ListItemSecondaryAction>
-                                </ListItem>
-                            </div>
-                        );
-                    })
-                ) : (null)}
-            </List>
-            {/* <Box className={classes.hGutter} /> */}
-        </Box>
-    );
+  return (
+    <Box width="100%" pl={{ sm: 1 }}>
+      <List disablePadding>
+        {bookmarks.statusOK ? (
+          bookmarks.data.map((v, i) => {
+            return (
+              <div key={i}>
+                <ListItem
+                  disableGutters
+                  button
+                  component="a"
+                  href={v.siteURL}
+                  target="_blank"
+                  rel="noopener"
+                  data-site-id={v.siteId}
+                  onClick={handleLastClicked}>
+                  <ListItemText
+                    className={classes.bookmarkText}
+                    primary={v.siteName}
+                    primaryTypographyProps={breakpointSM ? ({ variant: 'body1' }) : ({ variant: 'h5' })}
+                    secondary={v.category ? v.category : null}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton
+                      edge="end"
+                      data-site-id={v.siteId}
+                      onClick={handleEdit}
+                    ><MoreVertIcon /></IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              </div>
+            );
+          })
+        ) : (null)}
+      </List>
+      {/* <Box className={classes.hGutter} /> */}
+    </Box>
+  );
 };
 
 export default BookmarksComponent;
--- src/components/Content/NavigationComponent.js
+++ src/components/Content/NavigationComponent.js
@@ -15,76 +15,84 @@ import { getCategories } from '../../utilities/localstorage';
 import { context } from '../../context/StoreProvider';
 
 const NavigationComponent = ({ history }) => {
-    const [state, dispatch] = React.useContext(context);
-    const classes = useStyles();
-    const [categories, setCategories] = React.useState([]);
-    // const [active, setActive] = React.useState(state.activeNav);
+  const [state, dispatch] = React.useContext(context);
+  const classes = useStyles();
+  const [categories, setCategories] = React.useState([]);
+  const [search, setSearch] = React.useState('');
+  // const [active, setActive] = React.useState(state.activeNav);
 
-    React.useEffect(() => {
-        setCategories(getCategories());
-        return () => true;
-    }, []);
+  React.useEffect(() => {
+    setCategories(getCategories());
+    return () => true;
+  }, []);
 
-    const handleNav = (e, i) => {
-        const event = e.currentTarget.innerText;
-        // console.log('Navigation: innerText....', event);
-        switch (event) {
-            case 'Popular':
-                dispatch({ type: 'NAV', payload: 0 });
-                if (history.location.pathname !== '/') history.push('/');
-                break;
-            case 'Favourites':
-                dispatch({ type: 'NAV', payload: 1 });
-                if (history.location.pathname !== '/favourites') history.push('/favourites');
-                break;
-            default:
-                if (i !== undefined) dispatch({ type: 'NAV', payload: i + 2 });
-                if (history.location.pathname !== '/category/:id') history.push('/category/' + e.currentTarget.dataset.catId);
-                break;
-        }
-    };
+  const handleNav = (e, i) => {
+    const event = e.currentTarget.innerText;
+    // console.log('Navigation: innerText....', event);
+    switch (event) {
+      case 'Popular':
+        dispatch({ type: 'NAV', payload: 0 });
+        if (history.location.pathname !== '/') history.push('/');
+        break;
+      case 'Favourites':
+        dispatch({ type: 'NAV', payload: 1 });
+        if (history.location.pathname !== '/favourites') history.push('/favourites');
+        break;
+      default:
+        if (i !== undefined) dispatch({ type: 'NAV', payload: i + 2 });
+        if (history.location.pathname !== '/category/:id') history.push('/category/' + e.currentTarget.dataset.catId);
+        break;
+    }
+  };
 
-    // console.log('Nav: location...', history.location);
+  const handleSearch = (e) => {
+    // console.log('Nav: search...', e.currentTarget.value);
+    setSearch(e.currentTarget.value);
+  };
 
-    return (
-        <Paper>
-            <Box display={{ xs: 'none', sm: 'block' }}>
-                <Box className={classes.searchContainer}>
-                    <InputBase
-                        className={classes.searchField}
-                        placeholder="Search..."
-                    />
-                    <IconButton className={classes.searchButton}>
-                        <SearchIcon />
-                    </IconButton>
-                </Box>
-            </Box>
-            <List disablePadding>
-                <ListItem button disableGutters onClick={handleNav} selected={state.activeNav === 0}>
-                    <ListItemText primary="Popular" className={classes.listItemText} />
-                </ListItem>
-                <ListItem button disableGutters onClick={handleNav} selected={state.activeNav === 1}>
-                    <ListItemText primary="Favourites" className={classes.listItemText} />
-                </ListItem>
-                <Divider />
-                {categories.statusOK ? (
-                    categories.data.map((v, i) => {
-                        return (
-                            <div key={i}>
-                                <ListItem
-                                    button
-                                    disableGutters
-                                    data-cat-id={v.categoryId}
-                                    onClick={(e) => handleNav(e, i)}
-                                    selected={state.activeNav === (i + 2)}
-                                ><ListItemText primary={v.category} className={classes.listItemText} /></ListItem>
-                            </div>
-                        );
-                    })
-                ) : (null)}
-            </List>
-        </Paper>
-    );
+  // console.log('Nav: location...', history.location);
+
+  return (
+    <Paper>
+      <Box display={{ xs: 'none', sm: 'block' }}>
+        <Box className={classes.searchContainer}>
+          <InputBase
+            className={classes.searchField}
+            placeholder="Search..."
+            value={search}
+            onChange={handleSearch}
+          />
+          <IconButton className={classes.searchButton}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      <List disablePadding>
+        <ListItem button disableGutters onClick={handleNav} selected={state.activeNav === 0}>
+          <ListItemText primary="Popular" className={classes.listItemText} />
+        </ListItem>
+        <ListItem button disableGutters onClick={handleNav} selected={state.activeNav === 1}>
+          <ListItemText primary="Favourites" className={classes.listItemText} />
+        </ListItem>
+        <Divider />
+        {categories.statusOK ? (
+          categories.data.map((v, i) => {
+            return (
+              <div key={i}>
+                <ListItem
+                  button
+                  disableGutters
+                  data-cat-id={v.categoryId}
+                  onClick={(e) => handleNav(e, i)}
+                  selected={state.activeNav === (i + 2)}
+                ><ListItemText primary={v.category} className={classes.listItemText} /></ListItem>
+              </div>
+            );
+          })
+        ) : (null)}
+      </List>
+    </Paper>
+  );
 };
 
 export default NavigationComponent;
