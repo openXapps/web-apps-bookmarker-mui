5bd7d7d847c81096c8037311b7f5353958f49263
openXapps <gavin.dalton1@gmail.com>
2022-02-06 12:26:28 +0200

    Prevent flex wrap

--- src/components/Settings/SettingsStyles.js
+++ src/components/Settings/SettingsStyles.js
@@ -5,6 +5,7 @@ export const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
+    flexWrap: false,
     // marginTop: theme.spacing(3),
     padding: theme.spacing(2)
   },
