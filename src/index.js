81daf7c364f6c23635b73d09301948c7aa79ba12
openXapps <gavin.dalton1@gmail.com>
2022-05-28 13:29:33 +0200

    Align to RJS18 format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
 // import 'react-app-polyfill/ie11';
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import StoreProvider from './context/StoreProvider';
 
